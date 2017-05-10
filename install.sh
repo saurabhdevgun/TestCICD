b72fa870718e89103754ba00506bbbf6eed8a4e8
SHARMA Saurabhs 1 IMT/OLS <saurabhs1.sharma@orange.com>
2017-05-10 22:47:31 +0530

    script file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#javaOutput=$(java -version)
+#if ($javaOutput ==''); then
+##fi
+
+sudo apt-get install default-jdk -y
+sudo apt-get install tomcat7 -y
+
+
+
+#
+#output=$(mvn -version)
+#echo $output
